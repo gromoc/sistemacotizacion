38ec907717f016367eea1ebda0470add7f639b74
vgrados <vgrados2@gmail.com>
2017-11-18 01:20:54 -0500

    ACTUALIZANDO PROYECTO

--- util/functions.php
+++ util/functions.php
@@ -8,17 +8,6 @@ function add_js($js){
     }
 }
 
-<?php 
-$url="http://localhost:8080/Sys_Cotizacion/";
-define("base_url",$url);
-define("base_url_css", $url."public/css/");
-define("base_url_js", $url."public/js/");
-define("base_url_img", $url."public/images/");
-define("title","EMPRESA DE HENRRY HOLMES PAYTAN CARREÑO");
-define("CONTROLADOR_DEFECTO", "Usuarios");
-define("ACCION_DEFECTO", "index");
-
-?>
 
 
 ?>
