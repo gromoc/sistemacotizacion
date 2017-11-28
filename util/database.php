3ff47e15bda7f742b19668e7295057b16a162929
vgrados <vgrados2@gmail.com>
2017-11-28 11:34:16 -0500

    modificando conexion

--- util/database.php
+++ util/database.php
@@ -1,14 +1,20 @@
 <?php
 class Conectar{
 
+    protected static $db_host= "localhost";
+    protected static $db_user= "root";
+    protected static $db_pass= "";
+    protected static $db_name= "db_cotizacion";
+
+
     public function __construct(){
-        
     }
 
     public static function conexion(){
-        $conexion=new mysqli("localhost", "root", "", "db_cotizacion");
+        $conexion=new mysqli(self::$db_host, self::$db_user, self::$db_pass, self::$db_name);
         $conexion->query("SET NAMES 'utf8'");
         return $conexion;
     }
 }
 ?>
+
